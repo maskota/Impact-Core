8a59bc5affc06c0cf02b434dfd5b1a50515a14fe
Lex <50260829+4gname@users.noreply.github.com>
2021-08-12 04:26:31 +0700

    next work

--- src/main/java/com/impact/mods/gregtech/enums/IRecipeAdder.java
+++ src/main/java/com/impact/mods/gregtech/enums/IRecipeAdder.java
@@ -4,6 +4,7 @@ import net.minecraft.item.ItemStack;
 
 public interface IRecipeAdder {
 
-  public boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration,
-      int aEUt);
+    public boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration, int aEUt);
+
+    public boolean addPhotonContainer(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, int photonsAmount);
 }
--- src/main/java/com/impact/mods/gregtech/enums/RecipeAdder.java
+++ src/main/java/com/impact/mods/gregtech/enums/RecipeAdder.java
@@ -1,15 +1,22 @@
 package com.impact.mods.gregtech.enums;
 
+import com.impact.mods.gregtech.tileentities.multi.photonsystem.GTMTE_PhotonContainment;
 import com.impact.mods.gregtech.tileentities.multi.processing.defaultmachines.GTMTE_RailAssembler;
 import net.minecraft.item.ItemStack;
 
 public class RecipeAdder implements IRecipeAdder {
 
-  @Override
-  public boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration,
-      int aEUt) {
-    GTMTE_RailAssembler.sTrackAssemblerRecipes.addRecipe(true,
-        aInputs, new ItemStack[]{aOutput}, null, null, null, null, aDuration, aEUt, 0);
-    return true;
-  }
+    @Override
+    public boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration, int aEUt) {
+        GTMTE_RailAssembler.sTrackAssemblerRecipes.addRecipe(true,
+                aInputs, new ItemStack[]{aOutput}, null, null, null, null, aDuration, aEUt, 0);
+        return true;
+    }
+
+    @Override
+    public boolean addPhotonContainer(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, int photonsAmount) {
+        GTMTE_PhotonContainment.sPhotonContainer.addRecipe(true,
+                aInputs, aOutputs, null, null, null, null, aDuration, aEUt, photonsAmount);
+        return true;
+    }
 }
