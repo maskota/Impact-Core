5cef04f43200f6cff1f9ee1df8b27cd9f097987e
4gname <domian844@gmail.com>
2021-09-02 18:27:26 +0700

    add me system provider

--- src/main/java/com/impact/mods/gregtech/enums/IRecipeAdder.java
+++ src/main/java/com/impact/mods/gregtech/enums/IRecipeAdder.java
@@ -4,7 +4,9 @@ import net.minecraft.item.ItemStack;
 
 public interface IRecipeAdder {
 
-    public boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration, int aEUt);
+    boolean addTrackAssemblerRecipe(ItemStack[] aInputs, ItemStack aOutput, int aDuration, int aEUt);
 
-    public boolean addPhotonContainer(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, int photonsAmount);
+    boolean addPhotonContainer(ItemStack[] aInputs, ItemStack[] aOutputs, int aDuration, int aEUt, int photonsAmount);
+
+    boolean addMESPRecipes(ItemStack[] aInputs, ItemStack aOutput, int aDuration, int aEUt, int photonsAmount);
 }
